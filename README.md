3107035097a372cfcd3bbeb09a62d1185782663d
Marcos Martins <cesmart@gmail.com>
2019-08-28 19:01:57 -0300

    Adicionado programa que efetua a soma dois números reais

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-mackenzie-ads-python
-# mackenzie-ads-python
+#Algorítmos e Programação I
+
+## Atividade Avaliativa Semana 2
+
+### Programa que efetua a soma de dois números reais
+
+1. Execute a ação abaixo para executar o programa:
+
+```bash 
+python soma_dois_numeros.py
+```
+
+
---
+++ soma_dois_numeros.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+#Disciplina: Algorítmos e Programação I
+#Função: Calcular a multiplicação entre dois números reais
+#Autor: Marcos César Martins
+#Data: 28/08/2019
+#Captura o primeiro número
+num_1 = float(input('Digite o primeiro número: '))
+
+#Captura o segundo número
+num_2 = float(input('Digite o segundo número: '))
+
+#Realiza a multiplicação dos dois números capturados
+resultado = num_1 * num_2
+
+#Exibe o resultado da multiplicação
+print('Multiplicação = ' + str(resultado))
